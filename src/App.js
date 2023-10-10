ace30d814ff6bc1f605ea0ae778c461ce2e6e969
kitou88C39 <kanfutrooper@icloud.com>
2023-10-10 15:24:41 +0900

    src/App.js 編集

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import imageGrallery from './imageGrallery';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefalt();
+    console.log(ref.current.value);
+  };
+
   return (
     <div className='container'>
       <h2>My Pixabay</h2>
-      <form>
-        <input type='text' placeholder='画像を探す' />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='画像を探す' ref={ref} />
       </form>
       <imageGrallery />
     </div>
