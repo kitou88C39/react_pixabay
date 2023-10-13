566b602d9323ce6edb59abb89166df5f8d073c3c
kitou88C39 <kanfutrooper@icloud.com>
2023-10-14 08:28:59 +0900

    src/Grallery.js 編集

--- src/Grallery.js
+++ src/Grallery.js
@@ -6,11 +6,10 @@ const Gallery = ({ fetchData }) => {
     <div>
       <div className='images-wrapper'>
         {fetchData.map((data) => (
-          <div className='image'>
-            <img
-              src='https://pixabay.com/get/g3a2fe2595f07e068b7bbb71a8edbaf1339e41b9a42bb65a2d5310e1c8581e117e9a6717d6ae5f5ae1139db6ac6c104a1052296ab6c38f855fd807099711109ab_1280.jpg'
-              alt=''
-            />
+          <div className='image' key={data.id}>
+            <a href={data.pageURL} target='_blank' rel='noreferrer'>
+              <img src={data.largeImageURL} alt='' />
+            </a>
           </div>
         ))}
       </div>
